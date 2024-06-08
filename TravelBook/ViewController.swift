038081d45ba6a6455c5253e0cc8184eff6a49c20
sumeyrademirtas <smyrdmrts@gmail.com>
2024-06-08 08:24:48 +0300

    feat: Stop location updates and center map on selected place
    - Added locationManager.stopUpdatingLocation() to stop location updates when a place is selected.
    - Configured map view to center on the selected place's coordinates with a specific zoom level.
    - Added if condition inside locationManager function to handle specific logic when updating locations.

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -90,6 +90,14 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                                         mapView.addAnnotation(annotation)
                                         nameText.text = annotationTitle
                                         commentText.text = annotationSubtitle
+                                        
+                                        locationManager.stopUpdatingLocation()
+                                        
+                                        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                        let region = MKCoordinateRegion(center: coordinate, span: span)
+                                        mapView.setRegion(region, animated: true)
+                                        
+                                        
                                     }
                                 }
                             }
@@ -132,11 +140,13 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        //CLLocation enlem ve boylam barindiran obje.
-        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01) //span = zoom seviyesi
-        let region = MKCoordinateRegion(center: location, span: span)
-        mapView.setRegion(region, animated: true)
+        if chosenPlace == "" {
+            //CLLocation enlem ve boylam barindiran obje.
+            let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01) //span = zoom seviyesi
+            let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
     
